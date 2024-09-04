2205515608f75cd7fd20532c1a5bae48d1d1fbca
tuanh <tuanh101001@gmail.com>
2024-09-04 15:24:11 +0700

    update unit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,12 +20,12 @@ class TestAPI(TestCase):
         """Tests the behavior of append_pool function.
 
         Expected outputs:
-            A post request to create a new pool with pool id 98 and pool values of [1, 2, 3, 4, 5]
+            A post request to create a new pool with pool id 97 and pool values of [1, 2, 3, 4, 5]
             should return status code 200 with message 'inserted'.
         """
         response = self.client.post(
             "/pools/append",
-            json={"poolId": 98, "poolValues": [1, 2, 3, 4, 5]},
+            json={"poolId": 97, "poolValues": [1, 2, 3, 4, 5]},
             headers={"Content-Type": "application/json"},
         )
         self.assertEqual(response.status_code, 200)
@@ -39,21 +39,21 @@ class TestAPI(TestCase):
         """Tests the behavior of append_pool function.
 
         Expected outputs:
-            - The first post request should create a new pool with pool id 99 and pool values of [11, 12, 13, 14]
+            - The first post request should create a new pool with pool id 98 and pool values of [11, 12, 13, 14]
             and return status code 200 with message 'inserted'.
-            - The second post request should append pool values of [15, 16, 17, 18] to pool id 99
+            - The second post request should append pool values of [15, 16, 17, 18] to pool id 98
             and return status code 200 with message 'appended'.
         """
 
         response_1 = self.client.post(
             "/pools/append",
-            json={"poolId": 99, "poolValues": [11, 12, 13, 14]},
+            json={"poolId": 98, "poolValues": [11, 12, 13, 14]},
             headers={"Content-Type": "application/json"},
         )
 
         response_2 = self.client.post(
             "/pools/append",
-            json={"poolId": 99, "poolValues": [15, 16, 17, 18]},
+            json={"poolId": 98, "poolValues": [15, 16, 17, 18]},
             headers={"Content-Type": "application/json"},
         )
         self.assertEqual(response_1.status_code, 200)
@@ -61,22 +61,36 @@ class TestAPI(TestCase):
         self.assertEqual(response_2.status_code, 200)
         self.assertEqual(response_2.json, {"message": "appended"})
 
+    @patch(
+        "pools.Pools.data_filepath",
+        tempfile.NamedTemporaryFile(suffix=".json").name,
+    )
     def test_query_pool_happy_case(self):
         """Tests the behavior of query_pool function with an existent pool id.
 
         Expected outputs:
-            A post request to query for pool id 98 and 50th percentile should return status code 200,
-            with calculated percentile value is 3 and total count of values in pool is 5.
+            - The first post request should create a new pool with pool id 99 and pool values of [1, 2, 3, 4, 5]
+            and return status code 200 with message 'inserted'.
+            - The second post request to query for pool id 99 and 50th percentile should return status code 200,
+            with calculated percentile value is 5 and total count of values in pool is 5.
         """
-        response = self.client.post(
-            "/pools/query",
-            json={"poolId": 98, "percentile": 50},
+        response_v1 = self.client.post(
+            "/pools/append",
+            json={"poolId": 99, "poolValues": [1, 3, 5, 7, 9]},
             headers={"Content-Type": "application/json"},
         )
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response_v1.status_code, 200)
+        self.assertEqual(response_v1.json, {"message": "inserted"})
+
+        response_v2 = self.client.post(
+            "/pools/query",
+            json={"poolId": 99, "percentile": 50},
+            headers={"Content-Type": "application/json"},
+        )
+        self.assertEqual(response_v2.status_code, 200)
         self.assertEqual(
-            response.json,
-            {"Calculated percentile value": 3, "Total count of elements": 5},
+            response_v2.json,
+            {"Calculated percentile value": 5, "Total count of elements": 5},
         )
 
     def test_query_pool_not_found(self):
