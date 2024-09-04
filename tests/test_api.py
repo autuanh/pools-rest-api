5aa4317f2bb66560a143484ca32e8bbae04c6a4d
tuanh <tuanh101001@gmail.com>
2024-09-04 15:58:46 +0700

    update unit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -20,12 +20,12 @@ class TestAPI(TestCase):
         """Tests the behavior of append_pool function.
 
         Expected outputs:
-            A post request to create a new pool with pool id 97 and pool values of [1, 2, 3, 4, 5]
+            A post request to create a new pool with pool id 41 and pool values of [1, 2, 3, 4, 5]
             should return status code 200 with message 'inserted'.
         """
         response = self.client.post(
             "/pools/append",
-            json={"poolId": 97, "poolValues": [1, 2, 3, 4, 5]},
+            json={"poolId": 41, "poolValues": [1, 2, 3, 4, 5]},
             headers={"Content-Type": "application/json"},
         )
         self.assertEqual(response.status_code, 200)
@@ -39,21 +39,21 @@ class TestAPI(TestCase):
         """Tests the behavior of append_pool function.
 
         Expected outputs:
-            - The first post request should create a new pool with pool id 98 and pool values of [11, 12, 13, 14]
+            - The first post request should create a new pool with pool id 42 and pool values of [11, 12, 13, 14]
             and return status code 200 with message 'inserted'.
-            - The second post request should append pool values of [15, 16, 17, 18] to pool id 98
+            - The second post request should append pool values of [15, 16, 17, 18] to pool id 42
             and return status code 200 with message 'appended'.
         """
 
         response_1 = self.client.post(
             "/pools/append",
-            json={"poolId": 98, "poolValues": [11, 12, 13, 14]},
+            json={"poolId": 42, "poolValues": [11, 12, 13, 14]},
             headers={"Content-Type": "application/json"},
         )
 
         response_2 = self.client.post(
             "/pools/append",
-            json={"poolId": 98, "poolValues": [15, 16, 17, 18]},
+            json={"poolId": 42, "poolValues": [15, 16, 17, 18]},
             headers={"Content-Type": "application/json"},
         )
         self.assertEqual(response_1.status_code, 200)
@@ -69,14 +69,14 @@ class TestAPI(TestCase):
         """Tests the behavior of query_pool function with an existent pool id.
 
         Expected outputs:
-            - The first post request should create a new pool with pool id 99 and pool values of [1, 2, 3, 4, 5]
+            - The first post request should create a new pool with pool id 43 and pool values of [1, 2, 3, 4, 5]
             and return status code 200 with message 'inserted'.
-            - The second post request to query for pool id 99 and 50th percentile should return status code 200,
+            - The second post request to query for pool id 43 and 50th percentile should return status code 200,
             with calculated percentile value is 5 and total count of values in pool is 5.
         """
         response_v1 = self.client.post(
             "/pools/append",
-            json={"poolId": 99, "poolValues": [1, 3, 5, 7, 9]},
+            json={"poolId": 43, "poolValues": [1, 3, 5, 7, 9]},
             headers={"Content-Type": "application/json"},
         )
         self.assertEqual(response_v1.status_code, 200)
@@ -84,7 +84,7 @@ class TestAPI(TestCase):
 
         response_v2 = self.client.post(
             "/pools/query",
-            json={"poolId": 99, "percentile": 50},
+            json={"poolId": 43, "percentile": 50},
             headers={"Content-Type": "application/json"},
         )
         self.assertEqual(response_v2.status_code, 200)
@@ -102,6 +102,6 @@ class TestAPI(TestCase):
         with self.assertRaises(ValueError):
             self.client.post(
                 "/pools/query",
-                json={"poolId": 100, "percentile": 50},
+                json={"poolId": 44, "percentile": 50},
                 headers={"Content-Type": "application/json"},
             )
