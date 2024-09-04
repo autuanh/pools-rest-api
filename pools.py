4e15ea3119e70ae6b0ed31a64deec35e5f70aeda
tuanh <tuanh101001@gmail.com>
2024-09-04 14:36:32 +0700

    use Pools class to handle load and save pool data

---
+++ pools.py
@@ -0,0 +1,38 @@
+import json
+import os
+
+
+class Pools:
+    """A class to represent pools.
+
+    Attributes:
+        data_filepath (str): The path to the JSON file.
+    Methods:
+        load(): Loads a JSON file into a list from data_filepath
+        save(data): Saves data to a JSON file at data_filepath
+    """
+
+    data_filepath = os.path.realpath(os.path.dirname(__file__)) + "/pool_data.json"
+
+    @classmethod
+    def load(cls) -> list:
+        """Loads a JSON file into a list.
+
+        Returns:
+            list: The parsed JSON data.
+            In case of an exception, an empty list is returned.
+        """
+        try:
+            return json.load(open(cls.data_filepath))
+        except Exception as e:
+            return []
+
+    @classmethod
+    def save(cls, data: list) -> None:
+        """Writes data to a JSON file.
+
+        Parameters:
+          data (list): The data to be written to the JSON file.
+        """
+        with open(cls.data_filepath, "w") as f:
+            json.dump(data, f, indent=4)
